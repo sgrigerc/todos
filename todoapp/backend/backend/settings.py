13b2713a093f9fa75c1d32854097ba8d6ebfcca9
sgrigerc <sgrigerc@gmail.com>
2022-06-09 23:11:26 +0300

    work from the front and refinement of the back

--- todoapp/backend/backend/settings.py
+++ todoapp/backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'rest_framework.authtoken',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -132,3 +133,9 @@ REST_FRAMEWORK = {
  'rest_framework.authentication.TokenAuthentication',
 ]
 }
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+
+
